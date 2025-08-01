f9a3c04dfad55dc59abce4c2ce705a27f1503d03
Eduard Ciofu <cfedward7@gmail.com>
2025-08-01 22:11:06 +0200

    fixed diversion import

--- Plugins/InventoryGrid/Source/InventoryGrid/Private/Widgets/Composite/IG_Leaf.dv-conflict.cpp
+++
@@ -1,9 +0,0 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Widgets/Composite/IG_Leaf.h"
-
-void UIG_Leaf::ApplyFunction(const FuncType Function)
-{
-	Function(this);
-}
